7584388b88f839479eee330bc3a68c32c260e1c1
Damien <damien@dam.io>
2021-02-26 11:00:08 +0100

    naive sort by count

--- core/models.py
+++ core/models.py
@@ -1,6 +1,18 @@
 from django.db import models
+from bs4 import BeautifulSoup
 
 class Webpage(models.Model):
     url = models.URLField(unique=True)
     html = models.TextField()
-    pagerank = models.FloatField()
+    pagerank = models.FloatField(default=0)
+
+    def title(self):
+        soup = BeautifulSoup(self.html, 'lxml')
+        return soup.title.text
+
+"""
+class WebpageTokenCount(models.Model):
+    webpage = models.ForeignKey(Webpage)
+    token = models.TextField()
+    count = models.IntegerField()
+"""
--- core/views.py
+++ core/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+from core.models import Webpage
 
+import tqdm
 
 def index(request):
     return render(request, 'index.html')
 
 
 def results(request):
-    results = Webpage.objects.all().order_by('-pagerank').limit(10)
+    token = request.GET['q'].lower()
+    url_count = {}
+    webpages = Webpage.objects.all()
+    for webpage in tqdm.tqdm(webpages):
+        soup = BeautifulSoup(webpage.html, 'lxml')
+        text = soup.text
+        count = len([1 for token2 in text.split() if token == token2.lower()])
+        url_count[webpage.url] = count
+    webpages = list(webpages)
+    webpages.sort(key=lambda page: -url_count[page.url])
     return render(request, 'results.html', {
+        'results': webpages[:20],
         'query': request.GET['q'],
     })
